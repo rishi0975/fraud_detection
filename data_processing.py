a67fb881153e59df0d4e029d45b8c10643fcbf86
rishi0975 <patelrushabh734@gmail.com>
2024-02-09 11:13:43 +0530

    added 3 new ml model code

---
+++ data_processing.py
@@ -0,0 +1,22 @@
+print ("adding all data processing code related to fraud detection")
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("Sum function:")
+
+def multiplication(a,b):
+    d=a*b
+    return d
+
+print("Multiplication function:")
+
+def division(a,b):
+    e=a/b
+    return a
+
+print("Division func:")
